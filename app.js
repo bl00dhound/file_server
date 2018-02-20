98d3672e2173d8f965874f45dfaf0929d959017e
Oleksii Kuznietsov <oleksii.kuznietsov@smartyads.com>
2018-02-20 17:48:44 +0200

    add list of files

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const sassMiddleware = require('node-sass-middleware');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
-
 const app = express();
 
 // view engine setup
@@ -28,8 +25,9 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
+app.use('/login', require('./routes/login'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const R = require('ramda');
+const path = require('path');
+const fs = require('fs');
 const router = express.Router();
 
+const storageDIR = path.join(__dirname, '..', 'storage');
+
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'SmartyAds file server' });
-});
+router.get('/', (req, res, next) =>
+  fs.readdir(storageDIR, (err, files) => {
+    if (err) return res.render('index', { files: 'error read folder' });
+    return res.render('index', { filter: R.filter(R.equals('.gitkeep'))(files) })
+
+  })
+);
 
 module.exports = router;
